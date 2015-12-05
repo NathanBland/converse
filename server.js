7c845307c173100359fbcade7a0fce23a1ef8dff
Nathan Bland <nathan.bland@gmail.com>
2015-12-04 21:23:30 -0700

    Updated readme with new features and tech. More still to be added. - Updated styles - Updated templates  - now includes templates - need to make routes'

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
-var express = require('express');
-var sassMiddleware = require('node-sass-middleware');
-var path = require('path');
+var express = require('express')
+var sass = require('node-sass-middleware')
+var path = require('path')
 
-var app = express();
+var app = express()
 
 // Configure sass
 
-/** Middleware not working currently.
-  * Use the following command instead:
-  * sass --watch sass:public/css
-  */
-
-/*
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'sass'),
-  dest: path.join(__dirname, 'public/css'),
-  debug: true,
-  outputStyle: 'compressed',
-  prefix: '/css'
-}));
-*/
+app.use(
+  sass({
+    root: __dirname,
+    indentedSyntax: true,
+    src: '/sass',
+    dest: '/public/css',
+    prefix: '/css',
+    debug: true
+  })
+)
 
 // Set static directory to /public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Set Jade as the view engine
-app.set('view engine', 'jade');
+app.set('view engine', 'jade')
 
 // Site-wide variables
-app.locals.sitename = 'Converse';
+app.locals.sitename = 'Converse'
 
 // Routes
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('index', {
     user: 'TempUser',
     pagetitle: 'Home Page'
-  });
-});
+  })
+})
 
 // Start listening
-app.listen(3000);
+app.listen(3000)
 
-console.log('[converse] app listening on 3000');
+console.log('[converse] app listening on 3000')

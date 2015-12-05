5222c61a6ef0941a1df63f9c672ac9495d175bd4
keawade <keawade@gmail.com>
2015-12-04 19:51:38 -0600

    sass implemented (see comment in server.js)

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
 
 var app = express();
 
-app.use(express.static('public'));
+// Configure sass
+
+/** Middleware not working currently.
+  * Use the following command instead:
+  * sass --watch sass:public/css
+  */
+
+/*
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/css'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/css'
+}));
+*/
+
+// Set static directory to /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set Jade as the view engine
 app.set('view engine', 'jade');
 
+// Site-wide variables
 app.locals.sitename = 'Converse';
 
+// Routes
 app.get('/', function(req, res){
   res.render('index', {
     user: 'TempUser',
@@ -14,6 +38,7 @@ app.get('/', function(req, res){
   });
 });
 
+// Start listening
 app.listen(3000);
 
 console.log('[converse] app listening on 3000');

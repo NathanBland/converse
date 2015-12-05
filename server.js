3094cfbf77947b1c3a15ae8627e24027cd1e692f
keawade <keawade@gmail.com>
2015-12-04 18:09:00 -0600

    Jade?

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+//app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
+  /*
+  res.render('index', {
+    user: 'Temp'
+  });
+  */
   res.send('Hello from Whiteboards! Welcome to Converse!');
 });
 

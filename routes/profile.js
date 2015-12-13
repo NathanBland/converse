0fededfcf16102adbf522f43e11da52f225e7c55
keawade <keawade@gmail.com>
2015-12-13 16:40:42 -0600

    Card styling (incomplete)

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,15 @@
 var express = require('express')
 var router = module.exports = express.Router()
-var bodyParser = require('body-parser')
-
-// Configure body-parser
-// var jsonParser = bodyParser.json() // Commented out until in use.
-var urlencodedParser = bodyParser.urlencoded({extended: false})
-// Using this method recommended from https://github.com/expressjs/body-parser#express-route-specific
 
 // These routes are all prefixed by /profile
-router.get('/', urlencodedParser, function (req, res) {
+router.get('/', function (req, res) {
   console.log(req.user)
   res.render('profile', {
     user: req.user
   })
 })
 
-router.post('/displayname', urlencodedParser, function (req, res) {
+router.post('/displayname', function (req, res) {
   req.user.set({
     displayName: req.body.displayname
   })
@@ -27,12 +21,12 @@ router.post('/displayname', urlencodedParser, function (req, res) {
   })
 })
 
-router.post('/password', urlencodedParser, function (req, res) {
+router.post('/password', function (req, res) {
   console.log(req.body)
   res.redirect('/profile')
 })
 
-router.post('/email', urlencodedParser, function (req, res) {
+router.post('/email', function (req, res) {
   req.user.set({
     email: req.body.email
   })
@@ -42,9 +36,4 @@ router.post('/email', urlencodedParser, function (req, res) {
     }
     return res.redirect('/profile')
   })
-})
-
-router.post('/details', urlencodedParser, function (req, res) {
-  console.log(req.body)
-  res.redirect('/profile')
 })
